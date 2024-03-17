a94834be87eb8eeda75011c28dcb9dae49b54903
Marcello Prado <marcellopradomuller@gmail>
2024-03-17 19:08:28 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Album albumDoQueen = new Album();
+albumDoQueen.Nome = "A night at the opera";
+
+Musica musica1 = new Musica();
+musica1.Nome = "love of my life";
+musica1.Duracao = 213;
+
+Musica musica2 = new Musica();
+musica2.Nome = "Bohemian Rhapsody";
+musica2.Duracao = 354;
+
+albumDoQueen.AdicionarMusica(musica1);
+albumDoQueen.AdicionarMusica(musica2);
+
+albumDoQueen.ExibirMusicasDoAlbum();
+
+Banda queen = new Banda();
+queen.Nome = "Queem";
+queen.AdicionarAlbum(albumDoQueen);
+queen.ExibirDiscografia();
